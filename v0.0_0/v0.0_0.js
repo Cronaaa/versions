ff3608f7ced86cb5c90a46e7a980e8b2bcee8857
apetor <onetiu_rendy@yahoo.com>
2021-04-20 16:44:11 +0300

    3rd

---
+++ v0.0_0/v0.0_0.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "hellloo";
+  }
+
+function numar(){
+return (Math.floor(Math.random() * 11));}
+
+function cai() {
+    document.getElementById("ho").innerHTML = numar();
+  }
